0ef8392e7279339a8e62a30f19cf8d29ffaa00d4
김도윤 <kdy0096@gmail.com>
2026-04-17 00:32:22 +0900

    Initial Commit

---
+++ MBox/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MBox
+//
+//  Created by yoonie on 4/17/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MBox/MBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  MBoxApp.swift
+//  MBox
+//
+//  Created by yoonie on 4/17/26.
+//
+
+import SwiftUI
+
+@main
+struct MBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
