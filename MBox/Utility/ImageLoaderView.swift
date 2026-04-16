cb6c16db6f1ae7555cfdb3676a578ed12f866ec7
김도윤 <kdy0096@gmail.com>
2026-04-17 01:30:37 +0900

    Utility-CustomAlignment ImageloaderView

---
+++ MBox/Utility/ImageLoaderView.swift
@@ -0,0 +1,20 @@
+//
+//  ImageLoaderView.swift
+//  MBox
+//
+//  Created by yoonie on 4/17/26.
+//
+
+import SwiftUI
+
+
+/// URL을 통해 이미지를 비동기적으로 로드해서 SwiftUI View를 표시하는 커스텀 뷰
+struct ImageLoaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImageLoaderView()
+}
